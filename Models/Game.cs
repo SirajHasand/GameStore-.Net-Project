ecfed954d03ff4a3c849ce74f6353533cd356ecd
sirajhasand <sirajuddinhasand@gmail.com>
2026-03-01 22:37:29 +0430

    Add properties to Game model: Genre, GenreId, Price, and ReleaseDate

--- Models/Game.cs
+++ Models/Game.cs
@@ -6,4 +6,12 @@ public class Game
 {
     public int Id { get; set; }
     public required string Name { get; set; }
+
+    public Genre? Genre { get; set; }
+
+    public int GenreId { get; set; }
+
+    public decimal Price { get; set; }
+
+    public DateOnly ReleaseDate { get; set; }
 }
